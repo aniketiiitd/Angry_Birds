a71d4a907da65ce9e2c8573d441770abcc7e8f3b
gopaltiwari04 <142602636+gopaltiwari04@users.noreply.github.com>
2024-11-24 04:10:23 +0530

    Delete core/src/main/java/com/github/angry_bird_phy directory

--- core/src/main/java/com/github/angry_bird_phy/Level.java
+++
@@ -1,8 +0,0 @@
-package com.github.angry_bird_phy;
-
-import com.badlogic.gdx.physics.box2d.World;
-
-public interface Level {
-
-    public void createlevel(LevelScreen levelScreen, World world);
-}
