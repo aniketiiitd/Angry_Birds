f3f35a6ef4e59472bdcb79bdf099d92525694ef8
aniketiiitd <aniket23087@iiitd.ac.in>
2024-11-27 16:09:48 +0530

    removed_bugs

---
+++ Junit_Testing/TestRunner.java
@@ -0,0 +1,18 @@
+//package com.github.angry_bird.Junit_Testing;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+    public static void main(String[] args) {
+
+        System.out.println("\nSimulating Login Test...");
+        Result result = JUnitCore.runClasses(Testing.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println(result.wasSuccessful());
+
+    }
+}
---
+++ Junit_Testing/Testing.java
@@ -0,0 +1,13 @@
+//package com.github.angry_bird.Junit_Testing;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class Testing {
+
+    @Test
+    public void testme() {
+        assertTrue(true);
+
+    }
+}
